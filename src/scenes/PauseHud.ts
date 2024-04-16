c733f3c2ecc763bff5d75d23959f4c0137742fe4
martorelliantoni0 <martorelliantonio3@icloud.com>
2024-04-16 10:18:04 +0200

    sesso

--- src/scenes/PauseHud.ts
+++ src/scenes/PauseHud.ts
@@ -23,14 +23,7 @@ export default class PauseHud extends Phaser.Scene{
         this.continua=this.add.image(1024/2,300-20,"continua").setInteractive().on("pointerdown",()=>{
             if(PauseHud.level==1){
                 this.scene.resume("Level1");
-            }else if(PauseHud.level==2){
-                this.scene.resume("Level2");
-            }else if(PauseHud.level==3){
-                this.scene.resume("Level3");
-            }else if(PauseHud.level==4){
-                this.scene.resume("Boss");
             }
-            
             this.base.setAlpha(0);
             this.continua.setAlpha(0);
             this.esci.setAlpha(0);
@@ -42,10 +35,7 @@ export default class PauseHud extends Phaser.Scene{
             this.esci.setAlpha(0);
             this.scene.setVisible(false,"Overlay");
             this.scene.setVisible(false,"Level1");
-            this.scene.setVisible(false,"Level2");
-            this.scene.setVisible(false,"Level3");
-            this.scene.setVisible(false,"Boss");
-            this.scene.start("LevelSelection");
+            this.scene.start("Intro");
             }
         ).setOrigin(0.5,0.5).setDepth(14).setScale(0.3).setAlpha(1);
     };
